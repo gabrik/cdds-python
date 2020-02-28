efd2e23a32308ee4315030ea459228beff596a0b
Firas Sahli <firas.sahli@adlinktech.com>
2020-02-28 14:51:00 +0100

    update basic functionalities of the writer entity and build basic tests for the basic implementation for write and read
    Signed-off-by: Firas Sahli <firas.sahli@adlinktech.com>

---
+++ pydds/writer.py
@@ -0,0 +1,68 @@
+from .runtime import Runtime
+from .dds_binding import *
+import jsonpickle
+
+from pydds import *
+
+class Writer (Entity):
+    def __init__(self, pub, topic, ps = None, listener = None):
+        self.rt = Runtime.get_runtime()
+        self.participant = pub.participant
+        self.parent = pub
+        self.topic = topic
+        qos = self.rt.to_rw_qos(ps)
+        
+        self.keygen = self.topic.gen_key
+        
+        self._qos = qos
+        
+        self.listener = listener
+        
+        self.handle = self.rt.ddslib.dds_create_writer(pub.handle, topic.handle, self.qos, self.listener)
+        assert (self.handle > 0)
+    
+    @property
+    def handle(self):
+        return super(Writer, self).handle
+    
+    @handle.setter
+    def handle(self, entity):
+        super(Writer, self.__class__).handle.fset (self, entity)
+        
+    @property
+    def participant (self):
+        return super(Writer, self).participant
+    
+    @participant.setter
+    def participant(self, entity):
+        super(Writer, self.__class__).participant.fset (self, entity)
+        
+    @property
+    def qos(self):
+        return super(Writer, self).qos
+    
+    @qos.setter
+    def qos(self, qos):
+        super(Writer, self.__class__).qos.fset (self, qos)
+        
+    def write(self, s):
+        gk = self.keygen(s)
+        
+        kh = KeyHolder(gk)
+        
+        key = jsonpickle.encode(kh)
+        value = jsonpickle.encode(s)
+        
+        sample = DDSKeyValue(key.encode(), value.encode())
+        rc = self.rt.ddslib.dds_write(self.handle, byref(sample))
+        
+        if rc != 0:
+            raise(Exception("Write failed"))
+        return rc
+
+    def write_all(self, xs):
+        for x in xs:
+            self.write(x)
+
+    def dispose_instance(self, s):
+        self.rt.ddslib.dds_dispose(self.handle, byref(s))
---
+++ tests/testWriter.py
@@ -0,0 +1,95 @@
+import unittest
+
+__author__ = 'ADlink Technology'
+
+from pydds import *
+
+import os, sys
+
+from idl_parser import parser
+
+parser_ = parser.IDLParser()
+
+from idl_parser import parser
+import pydds.py_dds_utils as utils
+
+import time
+
+class DataWriterTest (unittest.TestCase):
+    def setUp(self):
+        self.rt = Runtime.get_runtime()
+        self.dp = Participant(0)
+        self.pub = Publisher(self.dp)
+        self.sub = Subscriber(self.dp)
+        
+        topic_name = "HelloWorldData_Msg"
+        type_support = self.rt.get_hello_world_simple_value_type_support()
+        
+        self.topic = self.dp.dds_create_topic(topic_name, type_support)
+        
+        self.writer = Writer(self.pub, self.topic, [Reliable(), KeepLastHistory(10)])
+        self.reader = Reader(self.sub, self.topic,  [Reliable(), KeepLastHistory(10)])
+        
+    def test_init_writer(self):
+        self.assertIsNotNone (self.writer, "Initializing the data_writer failed")
+        self.assertIsInstance(self.writer, Writer)
+        
+    def test_write(self):
+        idl_path = '/home/firas/cyclone/cdds_python/lexer/example.idl'
+        with open(idl_path, 'r') as idlf:
+            contents = idlf.read()
+            
+            global_module = parser_.load( contents)
+            my_module = global_module.module_by_name('HelloWorldData')
+            
+            MsgSeq = my_module.struct_by_name('Msg')
+        
+        className = "HelloWorldData_Msg"
+        HelloWorldData_Msg = utils.create_class(className , idl_path)
+        
+        newMsg = HelloWorldData_Msg(userID = 23, message = "Other message")
+        print("Writer >> Begin writeing data")
+        rc = self.writer.write(newMsg)
+        print("Writer >> Writing data completed")
+        time.sleep(5)
+        read_sample = False
+        print('reader>> begin read loop!')
+        while(not read_sample) :
+            time.sleep(1)
+            samples = self.reader.read(all_samples())
+            if samples is not None:
+                
+                cnt = 0
+                samples_as_list = list(samples)
+
+                for s in samples_as_list:
+                    if s[1].valid_data:
+                        sam = s[0]
+                        sam_new = cast(c_void_p(sam), POINTER(DDSKeyValue))
+                        if sam_new.contents.value is not None:
+                            v = sam_new.contents.value.decode(encoding='UTF-8')
+                            
+                            read_sample = True
+                            try:
+                                value = jsonpickle.decode(v)
+                                val_ = (HelloWorldData_Msg)(**value)
+                                print('********************************************************')
+                                print('******************Written data was**********************')
+                                print('val = {}'.format(val_))
+                                print('val_.userID {}'.format(val_.userID))
+                                print('val_.message {}'.format( val_.message))
+                                print('******************Read data was*************************')
+                                print("newMsg = {}".format(newMsg))
+                                print('newMsg.userID {}'.format( newMsg.userID))
+                                print('newMsg.message{}'.format( newMsg.message))
+                                print('********************************************************')
+
+                                self.assertEqual(val_, newMsg)
+                            except:
+                                raise Exception ("Invalid data received")
+                                
+
+    
+
+if __name__ == "__main__":
+    unittest.main() # run all tests
