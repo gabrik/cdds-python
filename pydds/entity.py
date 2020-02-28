0ba11f3fb531d5f733c9dd48f2cc7d5326315ae9
Firas Sahli <firas.sahli@adlinktech.com>
2020-02-28 14:03:38 +0100

    Basic implementation of the class Entity
    Signed-off-by: Firas Sahli <firas.sahli@adlinktech.com>

---
+++ pydds/entity.py
@@ -0,0 +1,96 @@
+from .runtime import Runtime
+from ctypes import  *
+from pydds import *
+
+class Entity(object):
+    def __init__(self):
+        self.rt = Runtime.get_runtime()
+        self._handle = None
+        self._parent = None
+        self._qos = None
+        self._listener = None
+        
+        self._participant = None
+        
+        
+    def dds_enable(self):
+        rc = self.rt.ddslib.dds_enable ( self.handle)
+        return rc
+    
+    def dds_delete(self):
+        rc = self.rt.ddslib.dds_delete()
+        return rc
+    
+
+    def dds_get_participant(self):
+        participant = None
+        if(self._handle is not None):
+            participant = self.rt.ddslib.dds_get_participant(self.handle)
+        return participant
+    
+    def dds_get_children(self, size = 0):
+        rc = 0
+        childern = {}
+        rc = self.rt.ddslib.dds_get_children(self.handle, childeren, size)
+            
+        if rc > 0 :
+             return childern
+        
+        return rc
+    
+    def dds_set_qos(self, qos = []):
+        rc = 0
+        rc = self.rt.ddslib.dds_set_qos(self.handle, self.qos)
+        return rc
+    
+    def dds_get_qos(self):
+        rc = 0
+        qos_policies = []
+        rc = self.rt.ddslib.dds_get_qos(self.handle, qos_policies)
+        
+        if(rc == 0):
+            return qos_policies
+        else:
+            return rc
+        
+    @property
+    def parent(self):
+        return self._parent
+    
+    @parent.setter
+    def parent(self, parent_entity):
+        self._parent = parent_entity
+        
+    @property 
+    def participant(self):
+        return self._participant
+    
+    @participant.setter
+    def participant(self, entity):
+        if ( entity is not None):
+            self._participant = entity
+    
+    @property
+    def qos(self):
+        return self._qos
+    
+    @qos.setter
+    def qos(self, qos):
+        rc = qos
+        return rc
+    
+    @property
+    def handle(self):
+        return self._handle
+    
+    @handle.setter
+    def handle(self, entity):
+        self._handle = entity
+    
+    @handle.deleter
+    def handle(self):
+        self.rt.ddslib.dds_delete(self._handle)
+        
+    def _check_handle(self):
+        if self.handle is None:
+            raise Exception('Entity is already closed')
