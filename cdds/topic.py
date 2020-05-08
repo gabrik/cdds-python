aad44cb489b2f2a6d2eb779c0e4d445f24d2df86
Firas Sahli <firas.sahli@adlinktech.com>
2020-05-08 15:15:48 +0200

    create basic functionalities for Topic Entity
    Signed-off-by: Firas Sahli <firas.sahli@adlinktech.com>

--- cdds/topic.py
+++ cdds/topic.py
@@ -1,38 +1,92 @@
 from .runtime import Runtime
 from .dds_binding import DDSKeyValue
+from ctypes import *
+
+from cdds import *
+from cdds.entity import *
+
+MAX_NAME_SIZE = 100
 
 class TopicType(object):
     def gen_key(self): None
-
-
-class FlexyTopic:
-    def __init__(self, dp, name, keygen=None, qos=None):
+    
+class Topic(Entity):
+    def __init__(self, dp, topic_name, type_support, data_type=None, qos=None):
         self.rt = Runtime.get_runtime()
+        self._name = topic_name.encode()
+        self.type_support = type_support
+        
+        self._qos = self.rt.to_rw_qos(qos)
+        self.handle = self.rt.ddslib.dds_create_topic(dp.handle, type_support, topic_name.encode(), self.qos, None)
+        
+        self.parent = dp
+        self.participant = dp
+        
+        keygen = None
+        
         if keygen is None:
             self.keygen = lambda x: x.gen_key()
         else:
             self.keygen = keygen
-
-        self.qos = self.rt.to_rw_qos(qos)
-        self.type_support = self.rt.get_key_value_type_support()
-        self.topic = self.rt.ddslib.dds_create_topic(dp.handle, self.type_support , name.encode(), self.qos, None)
-        self.handle = self.topic
-        assert (self.topic > 0)
-        self.data_type = DDSKeyValue
-        self.dp = dp
-
+            
+        self.data_type = self.type_name()
+        
+        assert (self.handle > 0)
+        
     def gen_key(self, s):
         return self.keygen(s)
-
-
-class Topic:
-    def __init__(self, dp, topic_name, type_support, data_type, qos):
+        
+    def get_name(self):
+        res = " "* MAX_NAME_SIZE
+        result = res.encode(encoding='utf_8', errors='strict')
+        rc = self.rt.ddslib.dds_get_name(self.handle, result, MAX_NAME_SIZE)
+         
+        if(rc >= 0):
+            return str(result.decode()).split('\x00', 1)[0]
+        else:
+            return ""
+    
+    @property 
+    def name(self):
+        return self._name.decode()
+    
+    @name.setter
+    def name(self, topic_name_str):
+        self._name = topic_name_str
+    
+    @property
+    def qos(self):
+        return self._qos
+    
+    @qos.setter
+    def qos(self, other_qos):
+        self._qos = other_qos
+        
+    def type_name(self):
+        res = " "* MAX_NAME_SIZE
+        result = res.encode(encoding='utf_8', errors='strict')
+        rc = self.rt.ddslib.dds_get_type_name(self.handle, result, MAX_NAME_SIZE)
+         
+        if(rc >= 0):
+            return str(result.decode()).split('\x00', 1)[0]
+        else:
+            return ""
+        
+    @property
+    def handle(self):
+        return super(Topic, self).handle
+    
+    @handle.setter
+    def handle(self, entity):
+        super(Topic, self.__class__).handle.fset (self, entity)
+        
+    def _check_handle(self):
+        if super().handle is None:
+            raise Exception('Entity is already closed')
+        
+class FoundTopic (Topic):
+    def __init__(self, participant):
         self.rt = Runtime.get_runtime()
-        self.topic_name = topic_name
-        self.type_support = type_support
-        self.data_type = data_type
-        self.qos = self.rt.to_rw_qos(qos)
-
-        self.topic = self.rt.ddslib.dds_create_topic(dp.handle, type_support, topic_name.encode(), self.qos, None)
-        self.handle = self.topic
-        assert (self.handle > 0)
+        
+        self.participant = participant
+        self.parent = participant 
