ac41bc6408397cf1b7160b28a9fcc91a0e26efca
Firas Sahli <firas.sahli@adlinktech.com>
2020-05-08 12:34:27 +0200

    Implemet basic functionalities for the publisher entity
    Signed-off-by: Firas Sahli <firas.sahli@adlinktech.com>

--- cdds/pub.py
+++ cdds/pub.py
@@ -1,7 +1,24 @@
 from .runtime import Runtime
 from .policy import Partition
 
-class Publisher:
+from cdds import *
+
+from cdds.writer import *
+
+class Publisher(Entity):
+    def __init__(self, dp, ps = None, listener = None):
+        self.rt = Runtime.get_runtime()
+        self.participant = dp
+        qos = None
+#       if ps is not None:
+#           qos = self.rt.to_ps_qos(ps)
+        qos = ps
+        
+        self.parent = dp
+        self._datawriter_list = []
+        self.handle = self.rt.ddslib.dds_create_publisher(self.participant.handle, qos, listener)
+        assert (self.handle is not None and self.handle > 0)
+        
     @staticmethod
     def partitions(ps):
         return [Partition(ps)]
@@ -9,13 +26,38 @@ class Publisher:
     @staticmethod
     def partition(p):
         return [Partition([p])]
+    
+    @property
+    def handle(self):
+        return super(Publisher, self).handle
+    
+    @handle.setter
+    def handle(self, entity):
+        super(Publisher, self.__class__).handle.fset (self, entity)
+        
+    def _check_handle(self):
+        if super().handle is None:
+            raise Exception('Entity is already closed')
+        return True
+    
+    @property
+    def participant (self):
+        return super(Publisher, self).participant
+    
+    @participant.setter
+    def participant(self, entity):
+        super(Publisher, self.__class__).participant.fset (self, entity)
 
-    def __init__(self, dp, ps = None):
-        self.rt = Runtime.get_runtime()
-        self.dp = dp
-        qos = None
-        if ps is not None:
-            qos = self.rt.to_ps_qos(ps)
-
-        self.handle = self.rt.ddslib.dds_create_publisher(self.dp.handle, qos, None)
-        assert (self.handle > 0)
+    def create_writer(self, topic, policy = None, dw_listener = None):
+        data_writer = Writer(self, topic, policy, dw_listener)
+        self._datawriter_list.append(data_writer)
+        return data_writer
+    
+    def suspend (self):
+        rc = self.rt.ddslib.dds_suspend(self.handle)
+        
+        return rc
+    
+    def resume(self):
+        rc = self.rt.ddslib.dds_resume(self.handle)
+        return rc
