21362c71c74ab447cfa596a8ba6292f5e120ef7f
Firas Sahli <firas.sahli@adlinktech.com>
2020-05-08 15:23:59 +0200

    Add new libraries to cdds module
    Signed-off-by: Firas Sahli <firas.sahli@adlinktech.com>

--- cdds/__init__.py
+++ cdds/__init__.py
@@ -1,8 +1,11 @@
 __author__ = 'Angelo Corsaro'
 
 from .runtime import Runtime
+from .policy import *
+from .entity import *
 from .participant import *
 from .topic import *
+from .flexy_topic import *
 from .sub import *
 from .pub import *
 from .reader import *
